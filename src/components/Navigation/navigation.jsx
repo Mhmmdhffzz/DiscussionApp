57753efbc084d1c5a4206ec944a0614dac527c5f
mhmmdhffzz <mhmmdhffzz@gmail.com>
2024-05-13 14:56:35 +0800

    add screenshot

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -26,7 +26,7 @@ function Navigation({ authUser, signOut }) {
         <Link to="/leaderboard" className="nav-link flex items-center">
           <AiOutlineTrophy className="mr-1 text-lg" />
           {/* Changed icon to AiOutlineTrophy */}
-          Leaderboards
+          Leaderboard
         </Link>
         </nav>
       <div className="nav__user-info flex items-center">
